8c655d4116194a10e84a122fac38eb9c8671ec37
SandyAssaam <60569380+SandyAssaam@users.noreply.github.com>
2020-02-24 02:06:22 -0800

    Add files via upload

---
+++ def.py
@@ -0,0 +1,48 @@
+# def thing():
+#     print('heloo')
+#     print('welcome')
+# thing()
+
+# x = 5
+# print('python')
+# def thing():
+#     print('hello')
+#     print('welcome')
+# x = x + 2
+# thing()
+# print(x)
+
+# def greet(lang):
+#     if lang == 'es':
+#         print('hola')
+#     elif lang == 'fr':
+#         print('bonjour')
+#     else:
+#         print('hello')
+# greet('ar')
+
+# def greet(lang):
+#     if lang == 'es':
+#         return 'hola'
+#     elif lang == 'fr':
+#         return 'bonjour'
+#     else:
+#         return 'hello'
+# print(greet('en'), 'sandy')
+
+# def greet(lang):
+#     if lang == 'es':
+#         return 'hola'
+#     elif lang == 'fr':
+#         return 'bonjour'
+#     else:
+#         return 'hello'
+# name = input('add your name:')
+# ylang = input('add your language:')
+# print(greet(ylang), name)
+
+
+def calc(a, b):
+    x = a + b
+    return x
+print(calc(2,3))
